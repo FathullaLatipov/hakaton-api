8583719700d56a94de5c4d40dbeb1b7ecf6f4e5b
FathullaLatipov <fathullaxonlatipov@gmail.com>
2023-07-05 01:18:47 +0500

    new apis

--- hack/views.py
+++ hack/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 @api_view(['GET', 'POST'])
@@ -34,6 +34,36 @@ def get_api(request):
 
     if "Это не юридический вопрос" in full_text:
         full_text = "Это не юридический вопрос, поэтому я не могу ответить."
+
+    full_text = full_text.replace('\n', '\n\n')
+
+
+    return Response({'result': full_text}, status=status.HTTP_200_OK)
+
+
+@api_view(['GET', 'POST'])
+def get_new_api(request):
+    if request.method == 'GET':
+        # Обработка GET-запроса
+        prompt = request.GET.get('prompt', '')
+    elif request.method == 'POST':
+        # Обработка POST-запроса
+        prompt = request.data.get('prompt', '')
+    else:
+        # Возвращаем ошибку для других методов запроса
+        return Response({'error': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.5,  # Увеличим температуру для более разнообразных ответов
+        max_tokens=2000,  # Увеличим максимальное количество токенов для получения более полного текста
+        top_p=0.9,  # Увеличим значение top_p для более релевантных ответов
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+    )
+
+    full_text = response.choices[0].text.strip()
 
     full_text = full_text.replace('\n', '\n\n')
 
--- hack/yasg.py
+++ hack/yasg.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-from hack.views import get_api
+from hack.views import get_api, get_new_api
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -19,4 +19,5 @@ urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('get-api/', get_api, name='get_api'),
+    path('get-new-api/', get_new_api, name='get_new_api'),
 ]
